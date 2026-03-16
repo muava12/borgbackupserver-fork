8ef00f765f65d96c368d7e293379537c679eaedd
Marc Pope <marcpope@me.com>
2026-03-16 03:29:55 -0400

    Bump version to 2.17.9

--- agent/bbs-agent.py
+++ agent/bbs-agent.py
@@ -44,7 +44,7 @@ if not hasattr(subprocess, "run"):
     subprocess.run = _subprocess_run
     subprocess.CompletedProcess = _CompletedProcess
 
-AGENT_VERSION = "2.17.7"
+AGENT_VERSION = "2.17.9"
 BORG_PATH = None  # Resolved in get_system_info()
 IS_WINDOWS = sys.platform == "win32"
 
