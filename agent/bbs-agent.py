9e8cc483bfdde335de4f349abbe19be13a0fe558
Marc Pope <marcpope@me.com>
2026-03-18 15:13:35 -0400

    Bump version to 2.17.14

--- agent/bbs-agent.py
+++ agent/bbs-agent.py
@@ -44,7 +44,7 @@ if not hasattr(subprocess, "run"):
     subprocess.run = _subprocess_run
     subprocess.CompletedProcess = _CompletedProcess
 
-AGENT_VERSION = "2.17.13"
+AGENT_VERSION = "2.17.14"
 BORG_PATH = None  # Resolved in get_system_info()
 IS_WINDOWS = sys.platform == "win32"
 
